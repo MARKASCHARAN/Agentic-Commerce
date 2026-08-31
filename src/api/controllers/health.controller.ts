31df7ae491d02a0b1b934d9ca4fcd3a6c6d22296
MARKASCHARAN <markascharan@gmail.com>
2026-08-31 20:25:21 +0530

    refactor : remove legacy flat routes and dangling controllers

--- src/api/controllers/health.controller.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-import { getHealthStatus } from "../../application/services/health.service.js";
-
-export function getHealth(_req: Request, res: Response) {
-  const health = getHealthStatus();
-
-  res.json(health);
-}
